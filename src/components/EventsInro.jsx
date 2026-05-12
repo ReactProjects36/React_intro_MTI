615c41c3c814e4bf08b3bd8c2ac8d46f768eb465
RatnaTeja9 <ratnateja.ds@gmail.com>
2026-05-12 14:54:29 +0530

    use state hook

--- src/components/EventsInro.jsx
+++ src/components/EventsInro.jsx
@@ -1,12 +1,17 @@
-function eventsIntro() {
-  console.log("Button Clicked");
-}
+import { useState } from "react";
 
-export default function ClickMeButton() {
+export default function Switch() {
+        
+const [isOn, setIsOn] = useState(false);
+
+  function setSwitch() {
+    setIsOn(!isOn);
+  }
+  
   return (
     <div>
-      <h5>Intoduction to Events in React</h5>
-      <button onClick={eventsIntro} >Click Me</button>
+      <h1>The Light is : {isOn ? "On" : "Off"} </h1>
+      <button onClick = {setSwitch} >Switch</button>
     </div>
   );
 }
