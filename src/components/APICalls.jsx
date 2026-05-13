ab8a32bb203842cc609d6365494e89f89602171c
RatnaTeja9 <ratnateja.ds@gmail.com>
2026-05-13 14:55:03 +0530

    API Calls

--- src/components/APICalls.jsx
+++ src/components/APICalls.jsx
@@ -2,17 +2,17 @@ import { useState, useEffect } from "react";
 
 export default function ApiCalls() {
   let [joke, setJoke] = useState("");
-  let [punchline , setPunchline] = useState("")
+  let [punchline, setPunchline] = useState("");
   async function getJoke() {
     const URL = "https://official-joke-api.appspot.com/random_joke";
     let data = await fetch(URL);
     let result = await data.json();
     setJoke(result.setup);
-    setPunchline(result.punchline)
+    setPunchline(result.punchline);
   }
   return (
     <div>
-      <h3>Joke : {loke} </h3>
+      <h3>Joke : {joke} </h3>
       <p>Punchline : {punchline}</p>
       <button onClick={getJoke}>Get Joke </button>
     </div>
