3658981a4a04a80df9313d71348d0ee5c6a2d24f
RatnaTeja9 <ratnateja.ds@gmail.com>
2026-05-13 12:13:26 +0530

    Objects usage in usestate

--- src/components/EventsInro.jsx
+++ src/components/EventsInro.jsx
@@ -1,17 +1,25 @@
 import { useState } from "react";
 
-export default function Switch() {
-        
-const [isOn, setIsOn] = useState(false);
+export default function UseStateDict(params) {
+ let [obj1 , setObject] = useState({num1 : 10 , num2 : 20}) 
 
-  function setSwitch() {
-    setIsOn(!isOn);
-  }
-  
-  return (
-    <div>
-      <h1>The Light is : {isOn ? "On" : "Off"} </h1>
-      <button onClick = {setSwitch} >Switch</button>
-    </div>
-  );
-}
+ function increaseNum(num){
+        setObject({...obj1 , [num] : obj1[num] + 1})
+ }
+ 
+ return (
+        <>
+        <div>
+                <h1>Num 1 : {obj1.num1} </h1>
+                <button onClick={()=> increaseNum("num1")} >Increase</button> 
+        </div>
+
+                <div>
+                <h1>Num 2 : {obj1.num2} </h1>
+                 <button onClick={()=> increaseNum("num2")} >Increase</button> 
+        </div>
+ 
+ </>
+ )
+};
+
