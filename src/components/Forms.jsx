f337a2ec620cecc42f1a31cdfc86ce902adf66e2
RatnaTeja9 <ratnateja.ds@gmail.com>
2026-05-14 14:51:01 +0530

    forms 3

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -2,16 +2,20 @@ import { useState } from "react";
 
 export default function Forms() {
         let[formData , setFormData] = useState({name : "Tony" , password : "123"})
+        function handleInputChange(e){
+                let name = e.target.name
+                let value = e.target.value
+                setFormData({...formData , [name] : value})}
   return (
     <>
       <h1>React Forms</h1>
       <form action="">
         <label htmlFor="">Name : </label>
-        <input type="text" name="name" id="name" value={formData.name} placeholder="Give First Name" />
+        <input type="text" name="name" id="name" value={formData.name} placeholder="Give First Name" onChange={(e)=> handleInputChange(e)}/>
         <br />
         <br />
         <label htmlFor="password">Password : </label>
-        <input type="password" name="password" id="password" value={formData.password} placeholder="Give Password" />
+        <input type="password" name="password" id="password" value={formData.password} placeholder="Give Password" onChange={(e)=> handleInputChange(e)}/>
         <br />
         <br />
         <button>Submit</button>
